0e540c11343e96f0c9b6ba1860a421701636fb47
Sammac Technologies <jluisse@gmail.com>
2014-07-10 11:51:39 +0200

    Update subscriptor_2socket_control.cpp

--- subscriptor_2socket_control.cpp
+++ subscriptor_2socket_control.cpp
@@ -1,3 +1,23 @@
+ /*  
+   @Developer José Luis Samper Escudero <Jluisse@gmail.com>, 
+	      Joaquín Macanás Valera <macanas_92@hotmail.com
+   @date   Thu 10 Jul 2014 10:00:00
+
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 /************
 **** Este subscriptor se encarga de transmitir todos los comandos de control (independientemente de su procedencia)
 **** al coche. Para ello abre una comunicacion con el servidor de control en Arduino Mega.
