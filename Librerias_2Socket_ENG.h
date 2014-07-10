1c3991370741840937adaa7bee3b86b83e133eaf
Sammac Technologies <jluisse@gmail.com>
2014-07-10 11:46:54 +0200

    Update Librerias_2Socket_ENG.h

--- Librerias_2Socket_ENG.h
+++ Librerias_2Socket_ENG.h
@@ -1,4 +1,23 @@
+ /*  
+   @Developer José Luis Samper Escudero <Jluisse@gmail.com>, 
+	      Joaquín Macanás Valera <macanas_92@hotmail.com
+   @date   Thu 10 Jul 2014 10:00:00
 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 /******************
